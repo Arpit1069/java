77e3c4721ab7cde77c1ea9c6a90241ba2b08ffee
Arpit1069 <arpitvidhale08@gmail.com>
2023-01-24 13:13:33 +0530

    second

---
+++ course/Scan.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.*;
+
+public class Scan {
+    public static void main(String[] args) {
+        
+    {
+        System.out.println("May i know your name");
+        String name;
+        Scanner  s = new Scanner(System.in);
+        name = s.next();
+        System.out.println("Hello " + name);
+    }
+}
+}
---
+++ course/Sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Sum {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a, b, add;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        add = a + b;
+        System.out.println("sum is " + add);
+
+    }
+}
